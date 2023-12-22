b7d9a1998933c89b0ffc2506cf6b4695cb532c84
tranvantinhnbk <tranvantinhnbk@gmail.com>
2023-12-22 16:26:02 +0700

    fix precommit

--- github-blog/src/app/common-component/navbar/navbar.component.ts
+++ github-blog/src/app/common-component/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule]
 })
 export class NavbarComponent {
 
