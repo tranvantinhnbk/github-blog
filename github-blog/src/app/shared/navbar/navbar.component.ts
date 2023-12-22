c7fc0e058b9230cf7a5fc7d5cd6bfb86d09689f1
tranvantinhnbk <tranvantinhnbk@gmail.com>
2023-12-22 18:24:32 +0700

    add icon

--- github-blog/src/app/shared/navbar/navbar.component.ts
+++ github-blog/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [MatButtonModule]
+  imports: [MatButtonModule, HttpClientModule, InlineSVGModule]
 })
 export class NavbarComponent {
 
