a80bd1c25cc276bea3646fb16b8fb6bec40a64d3
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-01-20 20:04:24 +0700

    add route to mnist

--- github-blog/src/app/pages/projects/projects-routing.module.ts
+++ github-blog/src/app/pages/projects/projects-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MnistComponent } from './mnist/mnist.component';
 
 const routes: Routes = [
-  { path: '', component: MnistComponent },
+  { path: 'mnist', component: MnistComponent },
 ];
 
 @NgModule({
