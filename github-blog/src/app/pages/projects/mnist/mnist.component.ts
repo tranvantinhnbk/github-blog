92e5ce7246988df61d35c0d245a942a9c153db74
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-01-20 19:56:21 +0700

    add projects folder

---
+++ github-blog/src/app/pages/projects/mnist/mnist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mnist',
+  standalone: true,
+  imports: [],
+  templateUrl: './mnist.component.html',
+  styleUrl: './mnist.component.scss'
+})
+export class MnistComponent {
+
+}
