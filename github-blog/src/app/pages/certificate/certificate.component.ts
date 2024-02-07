81a872f4d6a1e11df42397c684b8f186de54f5c9
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-02-08 01:24:45 +0700

    create certificate page

---
+++ github-blog/src/app/pages/certificate/certificate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate',
+  standalone: true,
+  imports: [],
+  templateUrl: './certificate.component.html',
+  styleUrl: './certificate.component.scss'
+})
+export class CertificateComponent {
+
+}
