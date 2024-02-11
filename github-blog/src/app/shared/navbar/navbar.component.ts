700d8dcd78c93b0cbe922b1e0a3f290e9b09c3e3
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-02-12 00:21:47 +0700

    cahnge href too routerLink

--- github-blog/src/app/shared/navbar/navbar.component.ts
+++ github-blog/src/app/shared/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { Component, Input } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatMenuModule, MatButtonModule, MatIconModule]
+  imports: [CommonModule, MatMenuModule, MatButtonModule, MatIconModule, RouterModule]
 })
 export class NavbarComponent {
   @Input() position: 'sticky' | 'relative' = 'sticky'; // Default to sticky if not provided
