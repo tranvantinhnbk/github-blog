aa017375e10847f9b964f042151621f71ac3068f
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-02-12 13:15:07 +0700

    add url initialize binding value level

--- github-blog/src/app/pages/about-me/about-me.component.ts
+++ github-blog/src/app/pages/about-me/about-me.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import {MatButtonModule} from '@angular/material/button';
-
+import { ActivatedRoute } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 @Component({
   selector: 'app-about-me',
   standalone: true,
@@ -11,4 +12,18 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class AboutMeComponent {
   pdfSrc: string = '/assets/pdf/CV-Tran Van Tinh.pdf';
+
+  fragmentHash = new Map<string, string>([
+    ['CV', '/assets/pdf/CV-Tran Van Tinh.pdf'],
+    ['AT', '/assets/pdf/academic-transcript.pdf']
+
+  ])
+
+  constructor(private _router: ActivatedRoute) {
+    this._router.fragment.pipe(takeUntilDestroyed()).subscribe(
+      fragment => {
+        fragment && (this.pdfSrc = this.fragmentHash.get(fragment) || this.pdfSrc);
+      }
+    )
+  }
 }
