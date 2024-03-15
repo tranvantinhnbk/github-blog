19bdea18619eae6149f713dc545e26cbde7d5a06
tranvantinhnbk <tranvantinhnbk@gmail.com>
2024-03-15 23:34:49 +0700

    setup docker-build-face

--- face/main.py
+++ face/main.py
@@ -11,15 +11,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",  # Replace with your origins
-    "http://localhost:8080",
-    "http://localhost:54700",  # Replace with your origins
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
